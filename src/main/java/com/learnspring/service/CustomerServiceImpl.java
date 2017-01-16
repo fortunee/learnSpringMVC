a03f720a44aecf1dd4fd41223fd310e595242f10
fortune ikechukwu ekeruo <legalmody@gmail.com>
2017-01-16 16:04:08 +0100

    Sample App run application demo

--- src/main/java/com/learnspring/service/CustomerServiceImpl.java
+++ src/main/java/com/learnspring/service/CustomerServiceImpl.java
@@ -1,5 +1,21 @@
 package com.learnspring.service;
 
-public class CustomerServiceImpl {
+import java.util.List;
 
+import com.learnspring.model.Customer;
+import com.learnspring.repository.CustomerRepository;
+import com.learnspring.repository.HibernateCustomerRepositoryImpl;
+
+public class CustomerServiceImpl implements CustomerService {
+	
+	
+	private CustomerRepository customerRespository = new HibernateCustomerRepositoryImpl();
+	
+	/* (non-Javadoc)
+	 * @see com.learnspring.service.CustomerService#findAll()
+	 */
+	@Override
+	public List<Customer> findAll() {
+		return customerRespository.findAll();
+	}
 }
